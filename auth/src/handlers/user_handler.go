e6a22f00cbe19e6f1f2f7284bc33b63e4e27e662
zaheerAamir <aamirzaheer95@gmail.com>
2023-10-27 22:49:30 +0530

    Added Swagger Annotations for handler

--- auth/src/handlers/user_handler.go
+++ auth/src/handlers/user_handler.go
@@ -7,14 +7,25 @@ import (
 	"net/http"
 	"searchRecommend/auth/schema"
 	service "searchRecommend/auth/services"
+	"searchRecommend/auth/src/middlewares"
 )
 
 type UserHandler struct {
 	UserService *service.UserService
 }
 
+// @Summary SignUp user route
+// @Description User can create Account
+// @Tags users
+// @Accept json
+// @Produce json
+// @Param body body schema.UserDto true "Request body in JSON format"
+// @Success 201 {object} schema.UserSuccess
+// @Failure 409 {object}  schema.Error
+// @Router /createUser [post]
 func (handler *UserHandler) CreateUserHandler(w http.ResponseWriter, r *http.Request) {
 
+	w.Header().Set("content-type", "application/json")
 	req, errr := io.ReadAll(r.Body)
 	if errr != nil {
 		panic(errr.Error())
@@ -30,26 +41,45 @@ func (handler *UserHandler) CreateUserHandler(w http.ResponseWriter, r *http.Req
 	}
 
 	var error schema.Error
-	error.CODE = 400
-	error.STATUSTEXT = http.StatusText(400)
+	error.CODE = 409
+	error.STATUSTEXT = http.StatusText(error.CODE)
 	error.MESSAGE = "User already exists!"
 
 	if exists {
-		w.WriteHeader(201)
-		w.Write([]byte("User Created"))
+		var success schema.UserSuccess
+		success.CODE = 201
+		success.TEXT = http.StatusText(success.CODE)
+		success.MESSAGE = "User created Successfully!"
+
+		json, err := json.Marshal(success)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		w.WriteHeader(success.CODE)
+		w.Write(json)
 	} else {
 		json, err := json.Marshal(error)
 		if err != nil {
 			panic(err.Error())
 		}
 		w.WriteHeader(error.CODE)
-		w.Header().Set("content-type", "application/json")
 		w.Write(json)
 	}
 }
 
+// @Summary Login user route
+// @Description User can Login to their Account
+// @Tags users
+// @Accept json
+// @Produce json
+// @Param body body schema.UserLoginDto true "Request body in JSON format"
+// @Success 200 {object} schema.UserLoginSuccess
+// @Failure 401 {object}  schema.Error
+// @Router /login [post]
 func (handler *UserHandler) LoginUserHandler(w http.ResponseWriter, r *http.Request) {
 
+	w.Header().Set("content-type", "application/json")
 	req, errr := io.ReadAll(r.Body)
 	if errr != nil {
 		panic(errr.Error())
@@ -66,10 +96,9 @@ func (handler *UserHandler) LoginUserHandler(w http.ResponseWriter, r *http.Requ
 			panic(err.Error())
 		}
 		w.WriteHeader(error.CODE)
-		w.Header().Set("content-type", "application/json")
 		w.Write(json)
 	} else {
-		var resp schema.UserSucces
+		var resp schema.UserLoginSuccess
 		resp.CODE = 200
 		resp.TEXT = "SuccesFully logged In. Please use the access token to execute any bookAPI"
 
@@ -80,19 +109,28 @@ func (handler *UserHandler) LoginUserHandler(w http.ResponseWriter, r *http.Requ
 		resp.ACCES_TOKEN = accessToken
 		resp.REFRESH_TOKEN = refreshToken
 
-		w.WriteHeader(200)
 		json, err := json.Marshal(resp)
 		if err != nil {
 			panic(err.Error())
 		}
-		w.Header().Set("content-type", "application/json")
+		w.WriteHeader(200)
 		w.Write(json)
 	}
 
 }
 
+// @Summary Refresh Access token route
+// @Description User can Refresh the expired Access token from the Refresh token
+// @Tags users
+// @Accept json
+// @Produce json
+// @Param body body schema.RefreshTokenDTO true "Request body in JSON format"
+// @Success 200 {object} schema.AccessTokenSchema
+// @Failure 403 {object} schema.Error
+// @Router /token [post]
 func (handler *UserHandler) RefreshTokenHandler(w http.ResponseWriter, r *http.Request) {
 
+	w.Header().Set("content-type", "application/json")
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		panic(err.Error())
@@ -109,7 +147,7 @@ func (handler *UserHandler) RefreshTokenHandler(w http.ResponseWriter, r *http.R
 	var error schema.Error
 
 	if !check && token == "" {
-		log.Println(check)
+		log.Println("[User Handler] [Refresh Token Handler] Valid Refresh token:", check)
 		error.CODE = 403
 		error.STATUSTEXT = http.StatusText(error.CODE)
 		error.MESSAGE = "Access Denied! Refresh Token invalid 🛑"
@@ -119,48 +157,66 @@ func (handler *UserHandler) RefreshTokenHandler(w http.ResponseWriter, r *http.R
 			panic(err.Error())
 		}
 		w.WriteHeader(error.CODE)
-		w.Header().Set("content-type", "application/json")
 		w.Write(json)
 	} else {
 		var access_token schema.AccessTokenSchema
 		access_token.AccessToken = token
-		w.WriteHeader(200)
 		json, err := json.Marshal(access_token)
 		if err != nil {
 			panic(err.Error())
 		}
-		w.Header().Set("content-type", "application/json")
+		w.WriteHeader(200)
 		w.Write(json)
 	}
 }
 
+// @Summary Logout user route
+// @Description User can Logout of their Account
+// @Tags users
+// @Produce json
+// @Security bearerToken
+// @Success 204
+// @Failure 404 {object}  schema.Error
+// @Router /logout   [delete]
 func (handler *UserHandler) LogoutHandler(w http.ResponseWriter, r *http.Request) {
 
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		panic(err.Error())
-	}
-	var email schema.Logout
-	if err1 := json.Unmarshal(body, &email); err1 != nil {
-		panic(err1.Error())
-	}
-
-	check := handler.UserService.LogoutService(email.EMAIL)
+	w.Header().Set("content-type", "application/json")
 	var error schema.Error
-	if !check {
-		error.CODE = 404
-		error.STATUSTEXT = http.StatusText(error.CODE)
-		error.MESSAGE = "User Not Found! Email does not exist"
+	error.CODE = 400
+	error.STATUSTEXT = http.StatusText(400)
+	error.MESSAGE = "Bad request it is a DELETE route"
 
-		w.WriteHeader(error.CODE)
+	if r.Method != http.MethodDelete {
 		json, err := json.Marshal(error)
 		if err != nil {
 			panic(err.Error())
 		}
-		w.Header().Set("content-type", "application/json")
+		w.WriteHeader(error.CODE)
+		w.Write(json)
+
+	}
+
+	userID, ok := r.Context().Value(middlewares.UserIDKey).(int64)
+	log.Println(userID)
+
+	if !ok {
+		panic("UserID not found or has unexpected type")
+	}
+
+	check := handler.UserService.LogoutService(userID)
+	if !check {
+		error.CODE = 404
+		error.STATUSTEXT = http.StatusText(error.CODE)
+		error.MESSAGE = "User Not Found! User does not exist"
+
+		json, err := json.Marshal(error)
+		if err != nil {
+			panic(err.Error())
+		}
+		w.WriteHeader(error.CODE)
 		w.Write(json)
 	} else {
+
 		w.WriteHeader(204)
-		w.Write([]byte("User Successfully Logged Out!"))
 	}
 }
--- books/src/handlers/book_handler.go
+++ books/src/handlers/book_handler.go
@@ -17,6 +17,7 @@ type BookHandler struct {
 //healthCheck handler
 
 func (handler *BookHandler) Query(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
 	count := handler.Bookservice.DbService()
 	dum := fmt.Sprintf("Helllo: %d", count)
 
@@ -27,12 +28,22 @@ func (handler *BookHandler) Query(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("content-type", "application/json")
 	w.Write(json)
 }
 
+// @Summary GetBooks user route
+// @Description User can get the list of books with pagination
+// @Tags books
+// @Produce json
+// @Security bearerToken
+// @Param page_no query int true "Page number (default 1)"
+// @Param limit query int true "Number of itmes per page (default 10)"
+// @Success 200 {object} schema.Books
+// @Failure 400 {object}  schema.Error
+// @Router /getBooks  [get]
 func (handler *BookHandler) GetBooks(w http.ResponseWriter, r *http.Request) {
 
+	w.Header().Set("content-type", "application/json")
 	param := r.URL.Query()
 
 	page_no := param.Get("page_no")
@@ -48,17 +59,17 @@ func (handler *BookHandler) GetBooks(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatal(err.Error())
 		}
-		w.Header().Set("content-type", "application/json")
 		w.WriteHeader(error.CODE)
 		w.Write(json)
-	} else if page_no == "" || limit == "" {
+	} else if page_no == "0" {
+		error.MESSAGE = "Invalid Page number! Page number starts from 1."
 		json, err := json.Marshal(error)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
-		w.Header().Set("content-type", "application/json")
 		w.WriteHeader(error.CODE)
 		w.Write(json)
+
 	} else {
 
 		books, num_pages, page_no := handler.Bookservice.GetBooksService(limit, page_no)
@@ -67,12 +78,13 @@ func (handler *BookHandler) GetBooks(w http.ResponseWriter, r *http.Request) {
 		error.STATUSTEXT = http.StatusText(error.CODE)
 		error.MESSAGE = "Page number does exist"
 
-		if page_no > int(num_pages) || page_no < 1 {
+		if page_no > int(num_pages) {
+			errMessg := fmt.Sprintf("Incorrect page number value! Page nuber should not be greater that %d for limit %s", int(num_pages), limit)
+			error.MESSAGE = errMessg
 			json, err := json.Marshal(error)
 			if err != nil {
 				log.Fatal(err.Error())
 			}
-			w.Header().Set("content-type", "application/json")
 			w.WriteHeader(error.CODE)
 			w.Write(json)
 
@@ -83,7 +95,6 @@ func (handler *BookHandler) GetBooks(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 
-			w.Header().Set("content-type", "application/json")
 			w.Write(json)
 		}
 
@@ -100,11 +111,23 @@ func (handler *BookHandler) convJsonHelper(query []string, w http.ResponseWriter
 		log.Fatal(err.Error())
 	}
 
-	w.Header().Set("content-type", "application/json")
 	w.Write(json)
 }
 
+// @Summary FilterBooks user route
+// @Description User can filter books author or publisher name also sort books by number of pages or average rating
+// @Tags books
+// @Produce json
+// @Security bearerToken
+// @Param author query string false "Author name to filter by"
+// @Param publisher query string false "Publisher name to filter by"
+// @Param avg_rating query string false "Sort by average rating ASC or DESC"
+// @Param num_pages query string false "Sort by number of pages ASC or DESC"
+// @Success 200 {object} schema.Books
+// @Failure 400 {object}  schema.Error
+// @Router /filterBooks  [get]
 func (handler *BookHandler) FilterBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
 	param := r.URL.Query()
 
 	checkAvg_rating := param.Has("avg_rating")
@@ -126,7 +149,6 @@ func (handler *BookHandler) FilterBooks(w http.ResponseWriter, r *http.Request) 
 		if err != nil {
 			log.Fatal(err.Error())
 		}
-		w.Header().Set("content-type", "application/json")
 		w.WriteHeader(error.CODE)
 		w.Write(json)
 
@@ -135,7 +157,6 @@ func (handler *BookHandler) FilterBooks(w http.ResponseWriter, r *http.Request) 
 		if err != nil {
 			log.Fatal(err.Error())
 		}
-		w.Header().Set("content-type", "application/json")
 		w.WriteHeader(error.CODE)
 		w.Write(json)
 
